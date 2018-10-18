0f9c0d95aa0c171798216530148926cbd9dc6bb2
romielisse <romielisse@gmail.com>
2018-10-18 13:27:03 -0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args){
+    ArrayList<Person> people = new ArrayList<>();
+    Scanner in = new Scanner(System.in);
+    String addPeople = "";
+
+    do{
+      System.out.print("Enter name: ");
+      String name = in.nextLine();
+
+      System.out.print("Does this person like French fries (true or false): ");
+      boolean fries = in.nextBoolean();
+
+      System.out.print("Does this person like to sleep (true or false): ");
+      boolean sleep = in.nextBoolean();
+
+      System.out.print("Is this person adventurous (true or false): ");
+      boolean adventurous = in.nextBoolean();
+      in.nextLine();
+
+      people.add(new Person(name, fries, sleep, adventurous));
+
+      System.out.print("Add another person Y(yes) or N(no)? ");
+      addPeople = in.nextLine();
+
+      System.out.println();
+
+    } while(addPeople.equalsIgnoreCase("y"));
+
+    for(Person p : people){
+      System.out.println("\nName: " + p.getName());
+      System.out.println("Likes French fries: " + p.isLikeFrenchFries());
+      System.out.println("Likes sleeping: " + p.isLikeSleeping());
+      System.out.println("Is adventurous: " + p.isAdventurous());
+    }
+  }
+}
---
+++ src/Person.java
@@ -0,0 +1,48 @@
+public class Person {
+  private String name;
+  private boolean likeFrenchFries;
+  private boolean likeSleeping;
+  private boolean adventurous;
+
+  public Person() {
+  }
+
+  public Person(String name, boolean likeFrenchFries, boolean likeSleeping, boolean adventurous) {
+    this.name = name;
+    this.likeFrenchFries = likeFrenchFries;
+    this.likeSleeping = likeSleeping;
+    this.adventurous = adventurous;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public boolean isLikeFrenchFries() {
+    return likeFrenchFries;
+  }
+
+  public void setLikeFrenchFries(boolean likeFrenchFries) {
+    this.likeFrenchFries = likeFrenchFries;
+  }
+
+  public boolean isLikeSleeping() {
+    return likeSleeping;
+  }
+
+  public void setLikeSleeping(boolean likeSleeping) {
+    this.likeSleeping = likeSleeping;
+  }
+
+  public boolean isAdventurous() {
+    return adventurous;
+  }
+
+  public void setAdventurous(boolean adventurous) {
+    this.adventurous = adventurous;
+  }
+}
